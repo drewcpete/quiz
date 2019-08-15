7d96c3b7cdafc366a40be9752497422adf48a94e
Andrew Peterson and Ethan Samuels-Ellingson <andrew@>
2019-08-15 14:38:20 -0700

    somewhat functional quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
+  // $("#nameSelect").change(console.log(this.val()));
+
   $("#furnitureForm").submit(function(event){
     var favoriteColor = $("#color").val();
     favoriteColor = favoriteColor.slice(1);
     favoriteColor = parseInt(favoriteColor, 16);
+    var name = $("#nameSelect").val();
+    var furnitureInput = $("input:radio[name=flavor]:checked").val();
 
-    c
+    var result = (favoriteColor + name + furnitureInput) % 3;
+
+    if (result === 0){
+      $("#friendModal").show();
+    } else if (result === 1) {
+      $("#boatModal").show();
+    } else {
+      $("#tjModal").show();
+    }
+
+    $(".close").click(function() {
+      $(".modal").hide();
+    });
+
 
 
     console.log(favoriteColor);
